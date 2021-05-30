2d9a1a8e960458c51f6fe5970e37cbc9edcc2d6f
ProgramistycznyŚwir <programistycznyswir@gmail.com>
2021-05-30 13:45:53 +0200

    Setting primary keys via ApplicationDbContext.OnModelCreating()

--- Projekt-Przepisy/Data/ApplicationDbContext.cs
+++ Projekt-Przepisy/Data/ApplicationDbContext.cs
@@ -1,16 +1,36 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Projekt_Przepisy.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Projekt_Przepisy.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<FavouriteRecipe> Favourites { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<RecipeAssignedCategory> RecipeAssignedCategories { get; set; }
+        public DbSet<RecipeCategory> Categories { get; set; }
+        public DbSet<RecipeRating> Ratings { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<FavouriteRecipe>().HasKey("RecipeID", "UserID");
         }
     }
 }
