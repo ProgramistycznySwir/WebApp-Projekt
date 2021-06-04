5f172f2f5c40884b6c2af7184e2af688a5aa007c
DwieStronyMocy <igomidormid@gmail.com>
2021-06-04 21:33:20 +0200

    Przepis 1.2

--- Projekt-Przepisy/Pages/Przepis.cshtml.cs
+++ Projekt-Przepisy/Pages/Przepis.cshtml.cs
@@ -1,10 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using Microsoft.Extensions.Logging;
 using Projekt_Przepisy.Data;
 using Projekt_Przepisy.Models;
 
@@ -13,16 +17,26 @@ namespace Projekt_Przepisy.Pages
 {
     public class PrzepisModel : PageModel
     {
+        readonly ILogger<IndexModel> _logger;
+        readonly ApplicationDbContext _context;
+        readonly UserManager<IdentityUser> _userManager;
+
+        public Recipe przepis { get; private set; }
+
+        public PrzepisModel(ILogger<IndexModel> logger, ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _logger = logger;
+            _context = context;
+            _userManager = userManager;
+        }
 
        
         public void OnGet()
         {
+            int recipeID = Int16.Parse(Request.Query["ID"]); 
 
-            
-
-
-
-
+            przepis = _context.Recipes
+               .Where(recipe => recipe.ID == recipeID).First();
         }
     }
 }
