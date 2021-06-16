725a4cf153a9d41c6ff501b151c3ce332215089d
DwieStronyMocy <igomidormid@gmail.com>
2021-06-16 23:02:56 +0200

    cofnięte zmiany

--- Projekt-Przepisy/Pages/DodajPrzepis.cshtml.cs
+++ Projekt-Przepisy/Pages/DodajPrzepis.cshtml.cs
@@ -62,41 +62,7 @@ namespace Projekt_Przepisy.Pages
 
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,AccessRights,DOB,FirstName,LastName,NRIC,Nationality,StaffIdentity")] Recipe recipe)
-        {
-            if (ModelState.IsValid)
-            {
-                var files = HttpContext.Request.Form.Files;
-                foreach (var Image in files)
-                {
-                    if (Image != null && Image.Length > 0)
-                    {
-
-                        var file = Image;
-                        var uploads = Path.Combine("uploads\\img\\employees" , Image.FileName);
-
-                        if (file.Length > 0)
-                        {
-                            var fileName = ContentDispositionHeaderValue.Parse
-                                (file.ContentDisposition).FileName.Trim('"');
-
-                            System.Console.WriteLine(fileName);
-                            using (var fileStream = new FileStream(Path.Combine(uploads, file.FileName), FileMode.Create))
-                            {
-                                await file.CopyToAsync(fileStream);
-                                recipe.ImageLink = file.FileName;
-                            }
-
-
-                        }
-                    }
-                }
-            }
-        }    
-
-                public IActionResult OnPost()
+                public  IActionResult OnPost()
         {
             if (!ModelState.IsValid)
                 return Page();
@@ -107,14 +73,15 @@ namespace Projekt_Przepisy.Pages
                 instructionsText: instructionsText,
                 userID: _userManager.GetUserId(this.User),
                 //TODO: Implement image links propperly
-                imageLink: null
+                imageLink: imageLink
+
                 );
             //TODO: Bardzo du¿o _context.SaveChanges(), ale nie chce mi siê myœleæ nad tym jak to obejœæ, a dzia³a :)
             _context.Recipes.Add(newRecipe);
             _context.SaveChanges();
             newRecipe = _context.Recipes.First(r => r.IngredientsList == ingredientsList);
 
-            foreach (var categoryName in categoriesList.Split(' ').Select(str => str.ToLower()))
+            foreach (var categoryName in categoriesList.Split(' ')?.Select(str => str.ToLower()))
             {
                 var category = _context.Categories.FirstOrDefault(cat => cat.Name == categoryName);
                 if (category is null)
